c7b72654900ef601b6f49977e6819c143192c0f7
Benjamin Marguin <benjamin.marguin@makina-corpus.com>
2015-12-05 22:42:27 +0100

    Use popups instead of markers, and add dynamic sizing

--- src/main.js
+++ src/main.js
@@ -151,42 +151,95 @@ var data = [ // Fake data
                 value: 23
             }
         ]
+    },
+    {
+        position: [46, 2],
+        values: [
+            {
+                class: 'ps',
+                label: 'PS',
+                value: 12
+            },
+            {
+                class: 'lr',
+                label: 'LR',
+                value: 15
+            },
+            {
+                class: 'fn',
+                label: 'FN',
+                value: 13
+            },
+            {
+                class: 'eelv',
+                label: 'EELV',
+                value: 25
+            },
+            {
+                class: 'autres',
+                label: '...',
+                value: 37
+            }
+        ]
     }
 ];
 
 
 
-var map       = L.map('map');
-var tileLayer = new L.OSM.Mapnik();
+var map = L.map('map', { minZoom: 7 });
 
-map.setView([43.9583006, 0.6879448], 7);
+// var tileLayer = new L.OSM.Mapnik();
+var tileLayer = new L.tileLayer('https://cartodb-basemaps-a.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png');
 
 tileLayer.addTo(map);
 
+map.setView([47, 2], 7);
 
 data.forEach(function (dataset) {
-    var bars = document.createElement('placeholder');
+    var container = document.createElement('div');
+    container.className = 'container';
+
+    var title;
+    if (dataset.title) {
+        title = document.createElement('div');
+        title.className = 'title';
+        title.innerHTML = dataset.title;
+
+        container.appendChild(title);
+    }
+
+
+    var bars          = document.createElement('div');
+    var barWidth      = Math.round10(100 / dataset.values.length, -2);
+    var barsWidth     = dataset.values.length * 30;
+
+    bars.className = 'chart-container';
+    bars.style.width  = barsWidth + 'px';
+    bars.style.height = '75px';
 
     dataset.values.forEach(function (value) {
         var bar = document.createElement('div');
         bar.classList.add('bar');
+        if (value.value <= 20) bar.classList.add('small');
         bar.classList.add(value.class);
         bar.innerHTML    = '<span>' + value.label + '</span>';
         bar.style.height = value.value + '%';
+        bar.style.width  = barWidth + '%';
 
         bars.appendChild(bar);
     });
 
-    var myIcon   = L.divIcon({
-        className : 'chart-container',
-        iconAnchor: L.point(75, 75),
-        html: bars.innerHTML
+    container.appendChild(bars);
+
+    var myPopup = L.popup({
+        closeButton: false,
+        autoPan: false,
+        keepInView: false,
+        closeOnClick: false,
+        offset: L.point(0, 20)
     });
 
-    var myMarker = L.marker(dataset.position, {
-        icon: myIcon,
-        clickable: false
-    });
-
-    myMarker.addTo(map);
+    myPopup.setContent(container);
+    myPopup.setLatLng(dataset.position);
+    myPopup.addTo(map);
 });
